2b4b0bcab8edd086adfb3fc55d54ac10d233ba50
leeseowoo <leeseowoo.kr@gmail.com>
2024-04-16 02:29:14 +0900

    feat: create station info widget

--- lib/view/main_screen.dart
+++ lib/view/main_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:seou_jiyeong_chul/view/station_info_widget.dart';
 import 'package:seou_jiyeong_chul/viewmodel/main_view_model.dart';
 
 class MainScreen extends StatefulWidget {
@@ -35,33 +37,44 @@ class _MainScreenState extends State<MainScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.green.shade300,
         title: const Center(
           child: Text(
             '서우지영철',
+            style: TextStyle(fontWeight: FontWeight.w600),
           ),
         ),
       ),
       body: Column(
         children: [
-          TextField(
-            controller: _queryTextEditingController,
-            decoration: InputDecoration(
-              border: const OutlineInputBorder(),
-              hintText: '역 이름 검색',
-              suffixIcon: IconButton(
-                icon: const Icon(Icons.search),
-                onPressed: () async {
-                  final stationName = _queryTextEditingController.text;
-                  await widget._mainViewModel.searchStation(stationName);
-                },
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _queryTextEditingController,
+              decoration: InputDecoration(
+                border: const OutlineInputBorder(),
+                hintText: '역 이름 입력',
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.search),
+                  onPressed: () async {
+                    final stationName = _queryTextEditingController.text;
+                    await widget._mainViewModel.searchStation(stationName);
+                  },
+                ),
               ),
             ),
           ),
+
+          _queryTextEditingController.text == ""
+              ? const Text("")
+              : Text(
+                  '${_queryTextEditingController.text}역 검색 결과',
+                  style: const TextStyle(fontSize: 17),
+                ),
+
           Expanded(
             child: ListView(
                 children: widget._mainViewModel.stationArrivalList
-                    .map((e) => Text(e.toString()))
+                    .map((e) => StationInfoWidget(stationArrival: e))
                     .toList()),
           ),
         ],
---
+++ lib/view/station_info_widget.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:seou_jiyeong_chul/model/model/station_arrival.dart';
+
+class StationInfoWidget extends StatefulWidget {
+  final StationArrival _stationArrival;
+
+  const StationInfoWidget({
+    super.key,
+    required stationArrival,
+  }) : _stationArrival = stationArrival;
+
+  @override
+  State<StationInfoWidget> createState() => _StationInfoWidgetState();
+}
+
+class _StationInfoWidgetState extends State<StationInfoWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        child: SizedBox(
+          width: 300,
+          height: 100,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Row(
+                  children: [
+                    Text(widget._stationArrival.trainLineNm),
+                    const SizedBox(width: 24,),
+                    Text(widget._stationArrival.btrainSttus),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text('도착: ${widget._stationArrival.arvlMsg2}'),
+                    const SizedBox(width: 24,),
+                    Text(widget._stationArrival.updnLine),
+                    const SizedBox(width: 24,),
+                    Text('현재: ${widget._stationArrival.arvlMsg3}'),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
